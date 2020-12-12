dab92d19c4b62358da1fa19c7672eb971ee7f3d6
kejiojo <ojokeji@gmail.com>
2020-12-12 12:53:51 +0000

    fifth commit

--- app.js
+++ app.js
@@ -3,6 +3,14 @@
 const hamBurger = document.querySelector('.hamburger');
 const menu = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
+const slideShow = document.querySelector('.slideshow');
+const cockTails = document.querySelector('.cocktails');
+const ingredient = document.querySelector('ingredient');
+let i;
+let alcoholList;
+
+
+
 
 
 // event listeners
@@ -14,5 +22,62 @@ const links = document.querySelectorAll('.nav-links li');
    
 });
 
+
+// functions
+
+async function alcData() {
+    const cocktailData = await fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail");
+
+    const data = await cocktailData.json();
+    console.log(data);
+    
+
+    // for (var i = 0; i < 20; i++) {
+    //   alcoholList = data.drinks[i].strDrink;
+    // }
+
+    createAlcoholList(data.drinks);
+    const select = document.querySelector('select');
+    
+    select.addEventListener("change" , function(e) {
+        createAlcoholSlider(e, data.drinks)
+    });
+}
+
+alcData();
+
+;
+
+
+
+function createAlcoholList(item) {
+    cockTails.innerHTML = `
+    <select>
+    <option>select</option>
+    ${item.map(function (alcItem) {
+        return `<option value="${alcItem.strDrink}">${alcItem.strDrink}</option>`;
+    }).join('')}
+    </select>
+    `
+ 
+   
+}
+
+function createAlcoholSlider(e, item) {
+    const findValue = e.target.value;
+    const currentData = item.find(arr => arr.strDrink === findValue);
+    slideShow.innerHTML = 
+       `<div class="slide" style="background-image: url('${currentData.strDrinkThumb}')"></div>`
+}
+
+
+
+
+
+
+
+  
+
+ 
 
 
